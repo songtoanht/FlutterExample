6945ec51e45c0bf59594b7a4f188ae5fdfe46478
ToanSongNguyen <songtoanht@gmail.com>
2018-10-18 10:10:44 +0700

    add image

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,40 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-//          child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
-    );
-  }
 //  @override
 //  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: 'Flutter Demo',
-//      theme: new ThemeData(
-//        // This is the theme of your application.
-//        //
-//        // Try running your application with "flutter run". You'll see the
-//        // application has a blue toolbar. Then, without quitting the app, try
-//        // changing the primarySwatch below to Colors.green and then invoke
-//        // "hot reload" (press "r" in the console where you ran "flutter run",
-//        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-//        // counter didn't reset back to zero; the application is not restarted.
-//        primarySwatch: Colors.blue,
+//    final wordPair = WordPair.random();
+//    return MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+//        body: Center(
+////          child: Text('Hello World'),
+//          child: Text(wordPair.asPascalCase),
+//        ),
 //      ),
-//      home: new MyHomePage(title: 'Flutter Demo Home Page'),
 //    );
 //  }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -112,6 +112,9 @@ class _MyHomePageState extends State<MyHomePage> {
             new Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
+            ),
+            new Image.asset(
+                'assets/images/ship.jpg', fit: BoxFit.cover
             ),
           ],
         ),
